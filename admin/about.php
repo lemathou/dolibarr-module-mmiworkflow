cc5836cd8739350fdfa363722c85819a1d2a261d
Mathieu Moulin <mathieu@iprospective.fr>
2023-03-13 21:06:23 +0100

    Initialisation

---
+++ admin/about.php
@@ -0,0 +1,29 @@
+<?php
+/* Copyright (C) 2004-2017 Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) 2022 Mathieu Moulin <mathieu@iprospective.fr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file    mmiworkflow/admin/about.php
+ * \ingroup mmiworkflow
+ * \brief   About page of module MMIWorkflow.
+ */
+
+// Load Dolibarr environment
+require_once '../env.inc.php';
+require_once '../main_load.inc.php';
+
+require_once('../../mmicommon/admin/mmiabout_1.inc.php');
